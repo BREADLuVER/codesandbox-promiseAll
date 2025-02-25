76177d282d6da4d4aa05e62f03dc75d41ef54ee4
BREADLuVER <91650640+BREADLuVER@users.noreply.github.com>
2025-02-25 17:48:24 +0000

    flattened

--- src/App.js
+++ src/App.js
@@ -12,9 +12,18 @@ function flatten(arr) {
   return accum;
 }
 
+function flatten2(arr) {
+  const ans = arr.reduce(
+    (acum, curr) =>
+      Array.isArray(curr) ? acum.concat(flatten2(curr)) : acum.concat(curr),
+    []
+  );
+  return ans;
+}
+
 export default function App() {
   const nestedArray = [1, [2, [3, [4, 5]]], 6];
-  const flattened = flatten(nestedArray);
+  const flattened = flatten2(nestedArray);
   console.log(flattened);
 
   return (
@@ -22,7 +31,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <div>
-        <span>flattened</span>
+        <span>Flattened Array: {JSON.stringify(flattened)}</span>
       </div>
     </div>
   );
